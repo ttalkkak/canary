fae5c92be016a7e171fe80f4b506fdb1a82f2a6d
owjxyz <owj1110@icloud.com>
2024-08-18 16:47:10 +0900

    tab color for Safari

--- scripts/option.js
+++ scripts/option.js
@@ -1,5 +1,6 @@
 const fontSelector = document.getElementById('fontSelector');
 const themeSelector = document.getElementById('themeSelector');
+const tabColor = document.getElementById('theme-color');
 const body = document.querySelector('body');
 const textInput = document.getElementById('textInput');
 
@@ -27,7 +28,21 @@ fontSelector.addEventListener('change', changeFont);
 function changeTheme() {
     const theme = themeSelector.value;
     body.className = theme;
+    changeTabColor();
     localStorage.setItem('Theme', theme);
+}
+
+function changeTabColor() {
+    // Change tab color based on theme 
+    // (for Safari, Mobile Browsers, etc.)
+    const theme = themeSelector.value;
+    if (theme === 'dark') {
+        tabColor.setAttribute('content', '#343434');
+    } else if (theme === 'light') {
+        tabColor.setAttribute('content', '#f3f5f7');
+    } else if (theme === 'system') {
+        tabColor.setAttribute('content', 'auto');
+    }
 }
 
 if (savedTheme !== null) {
